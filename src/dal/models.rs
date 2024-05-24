ecd0e2e06caa4180c8a7c164f28ba314336bd300
koktezib <91209347+koktezib@users.noreply.github.com>
2024-05-24 07:09:56 +0300

    table_name change to diesel(table_name ="")

--- src/dal/models.rs
+++ src/dal/models.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 use crate::dal::schema::tasks;
 
 #[derive(Insertable, Selectable,QueryableByName,Queryable, Serialize, Deserialize, Debug)]
-#[table_name = "tasks"]
+#[diesel(table_name = tasks)]
 pub struct TaskEntity {
     pub id: i32,
     pub title: String,
